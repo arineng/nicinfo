83775677c096365ee6f4b2d7e855af62163e165f
Andy Newton <andy@arin.net>
2018-03-08 22:43:07 -0500

    maybe a working btree

--- spec/net_tree_spec.rb
+++ spec/net_tree_spec.rb
@@ -17,7 +17,7 @@ require 'rspec'
 require 'pp'
 require_relative '../lib/nicinfo/net_tree'
 
-describe 'bulk_infile test' do
+describe 'net_tree test' do
 
   it 'should btree basics' do
 
@@ -73,43 +73,9 @@ describe 'bulk_infile test' do
 
   end
 
-  it 'should do boundary tests' do
+  xit 'should insert' do
 
-    node = NicInfo::NetNode.new( "10.0.1.0/24" )
-
-    expect( node.include?( NicInfo::NetNode.new( "10.0.1.1" ) ) ).to be_truthy
-    expect( node.include?( NicInfo::NetNode.new( "9.0.0.1" ) ) ).to be_falsey
-    expect( node.include?( NicInfo::NetNode.new( "10.0.0.1" ) ) ).to be_falsey
-
-    expect( node.contained_by?( NicInfo::NetNode.new( "10.0.1.0/16" ) ) ).to be_truthy
-    expect( node.contained_by?( NicInfo::NetNode.new( "10.0.0.0/16" ) ) ).to be_truthy
-    expect( node.contained_by?( NicInfo::NetNode.new( "10.1.0.0/16" ) ) ).to be_falsey
-
-    expect( node.left_of?( NicInfo::NetNode.new( "10.0.3.0/24" ) ) ).to be_truthy
-    expect( node.left_of?( NicInfo::NetNode.new( "10.0.2.0/24" ) ) ).to be_truthy
-    expect( node.right_of?( NicInfo::NetNode.new( "10.0.0.0/24" ) ) ).to be_truthy
-
-    expect( node.exactly_matches?( NicInfo::NetNode.new( "10.0.1.0/24" ) ) ).to be_truthy
-
-    overlap = NicInfo::NetNode.new
-    overlap.begin = IPAddr.new( "10.0.0.255" ).to_i
-    overlap.end = IPAddr.new( "10.0.1.255" ).to_i
-    expect( node.overlaps?( overlap ) ).to be_truthy
-    overlap.begin = IPAddr.new( "10.0.1.0" ).to_i
-    overlap.end = IPAddr.new( "10.0.2.1" ).to_i
-    expect( node.overlaps?( overlap ) ).to be_truthy
-    overlap.begin = IPAddr.new( "10.0.1.0" ).to_i
-    overlap.end = IPAddr.new( "10.0.1.255" ).to_i
-    expect( node.overlaps?( overlap ) ).to be_truthy
-    overlap.begin = IPAddr.new( "10.0.1.1" ).to_i
-    overlap.end = IPAddr.new( "10.0.1.254" ).to_i
-    expect( node.overlaps?( overlap ) ).to be_falsey
-
-  end
-
-  it 'should insert' do
-
-    t = NicInfo::NetTree.new
+    t = NicInfo::NetBTree.new
     t.insert( "10.0.0.0/24", 1 )
     t.insert( "10.0.0.0/16", 2 )
     t.insert( "10.0.0.0/25", 3 )
