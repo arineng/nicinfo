0793d13e3b2436a78e669292e7a94055b6b6d864
Andy Newton <andy@arin.net>
2018-03-08 22:37:07 -0500

    maybe a working btree

--- lib/nicinfo/net_tree.rb
+++ lib/nicinfo/net_tree.rb
@@ -16,6 +16,83 @@ require 'ipaddr'
 
 module NicInfo
 
+  class NetBTree
+
+    BNode = Struct.new( :left, :right, :number, :data, :cidr_string, :ipaddr )
+
+    def new_bnode( cidr_string, data )
+      ipaddr = IPAddr.new( cidr_string )
+      number = ipaddr.to_range.begin.to_i
+      return BNode.new( nil, nil, number, data, cidr_string, ipaddr )
+    end
+
+    def put( node, cidr_string, data )
+      new_node = new_bnode( cidr_string, data )
+      return insert( node, new_node)
+    end
+
+    def insert( node, new_node )
+      if node == nil
+        return new_node
+      else
+        if new_node.number <= node.number
+          node.left = insert( node.left, new_node )
+        else
+          node.right = insert( node.right, new_node )
+        end
+        return node
+      end
+    end
+
+    def get( node, ip )
+      target = IPAddr.new( ip ).to_i
+      return lookup( node, target )
+    end
+
+    def lookup( node, target )
+      if node == nil
+        return nil
+      else
+        if target == node.number
+          return node
+        else
+          if target < node.number
+            return lookup( node.left, target )
+          else
+            return lookup( node.right, target )
+          end
+        end
+      end
+    end
+
+    def get_floor( node, ip )
+      target = IPAddr.new( ip ).to_i
+      return floor( node, target )
+    end
+
+    def floor( node, target )
+      if node == nil
+        return nil
+      else
+        if target == node.number
+          return node
+        else
+          if target < node.number
+            return floor( node.left, target )
+          else
+            f = floor( node.right, target )
+            if f
+              return f
+            else
+              return node
+            end
+          end
+        end
+      end
+    end
+
+  end
+
   class NetNode
 
     attr_accessor :left, :right, :begin, :end, :data, :cidr_string, :ipaddr
--- spec/net_tree_spec.rb
+++ spec/net_tree_spec.rb
@@ -19,6 +19,60 @@ require_relative '../lib/nicinfo/net_tree'
 
 describe 'bulk_infile test' do
 
+  it 'should btree basics' do
+
+    t = NicInfo::NetBTree.new
+    root = t.insert( nil, NicInfo::NetBTree::BNode.new( nil, nil, 5, "a", nil, nil ) )
+    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 3, "b", nil, nil ) )
+    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 1, "c", nil, nil ) )
+    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 4, "d", nil, nil ) )
+    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 9, "e", nil, nil ) )
+    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 6, "f", nil, nil ) )
+    expect( t.lookup( root, 1 ).data ).to eq( "c" )
+    expect( t.lookup( root, 3 ).data ).to eq( "b" )
+    expect( t.lookup( root, 4 ).data ).to eq( "d" )
+    expect( t.lookup( root, 5 ).data ).to eq( "a" )
+    expect( t.lookup( root, 6 ).data ).to eq( "f" )
+    expect( t.lookup( root, 9 ).data ).to eq( "e" )
+    expect( t.floor( root, 1 ).data ).to eq( "c" )
+    expect( t.floor( root, 2 ).data ).to eq( "c" )
+    expect( t.floor( root, 3 ).data ).to eq( "b" )
+    expect( t.floor( root, 4 ).data ).to eq( "d" )
+    expect( t.floor( root, 5 ).data ).to eq( "a" )
+    expect( t.floor( root, 6 ).data ).to eq( "f" )
+    expect( t.floor( root, 7 ).data ).to eq( "f" )
+    expect( t.floor( root, 8 ).data ).to eq( "f" )
+    expect( t.floor( root, 9 ).data ).to eq( "e" )
+    expect( t.floor( root, 10 ).data ).to eq( "e" )
+
+  end
+
+  it 'should do net btree stuff' do
+
+    t = NicInfo::NetBTree.new
+    root = t.new_bnode( "0.0.0.0/0", "a" )
+    t.put( root, "10.0.0.0/16", "b")
+    t.put( root, "9.0.0.0/16", "c")
+    t.put( root, "8.0.0.0/16", "d")
+    t.put( root, "11.0.0.0/16", "e")
+    expect( t.get( root, "10.0.0.0" ).data ).to eq( "b" )
+    expect( t.get( root, "9.0.0.0" ).data ).to eq( "c" )
+    expect( t.get( root, "8.0.0.0" ).data ).to eq( "d" )
+    expect( t.get( root, "11.0.0.0" ).data ).to eq( "e" )
+    expect( t.get_floor( root, "11.0.0.0" ).data ).to eq( "e" )
+    expect( t.get_floor( root, "11.0.0.1" ).data ).to eq( "e" )
+    expect( t.get_floor( root, "8.0.0.0" ).data ).to eq( "d" )
+    expect( t.get_floor( root, "8.0.0.1" ).data ).to eq( "d" )
+    expect( t.get_floor( root, "9.0.0.0" ).data ).to eq( "c" )
+    expect( t.get_floor( root, "9.0.0.1" ).data ).to eq( "c" )
+    expect( t.get_floor( root, "10.0.0.0" ).data ).to eq( "b" )
+    expect( t.get_floor( root, "10.0.0.1" ).data ).to eq( "b" )
+    expect( t.get_floor( root, "10.0.0.2" ).data ).to eq( "b" )
+    expect( t.get_floor( root, "10.0.0.255" ).data ).to eq( "b" )
+    expect( t.get_floor( root, "10.255.255.255" ).data ).to eq( "b" )
+
+  end
+
   it 'should do boundary tests' do
 
     node = NicInfo::NetNode.new( "10.0.1.0/24" )
