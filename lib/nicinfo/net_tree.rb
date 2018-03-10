8d87db5ea2c64758d341deafa70c1313c43cabc3
Andy Newton <andy@arin.net>
2018-03-10 13:49:56 -0500

    working net_tree

--- lib/nicinfo/net_tree.rb
+++ lib/nicinfo/net_tree.rb
@@ -16,90 +16,15 @@ require 'ipaddr'
 
 module NicInfo
 
-  class NetBTree
-
-    BNode = Struct.new( :left, :right, :number, :data, :cidr_string, :cidr_prefix, :cidr_length, :ipaddr )
-
-    def new_bnode( cidr_string, data )
-      ipaddr = IPAddr.new( cidr_string )
-      number = ipaddr.to_range.begin.to_i
-      cidr = cidr_string.split( "/" )
-      return BNode.new( nil, nil, number, data, cidr_string, cidr[0], cidr[1].to_i, ipaddr )
-    end
-
-    def put( node, cidr_string, data )
-      new_node = new_bnode( cidr_string, data )
-      return insert( node, new_node)
-    end
-
-    def insert( node, new_node )
-      if node == nil
-        return new_node
-      else
-        if new_node.number <= node.number
-          node.left = insert( node.left, new_node )
-        else
-          node.right = insert( node.right, new_node )
-        end
-        return node
-      end
-    end
-
-    def get( node, ip )
-      target = IPAddr.new( ip ).to_i
-      return lookup( node, target )
-    end
-
-    def lookup( node, target )
-      if node == nil
-        return nil
-      else
-        if target == node.number
-          return node
-        else
-          if target < node.number
-            return lookup( node.left, target )
-          else
-            return lookup( node.right, target )
-          end
-        end
-      end
-    end
-
-    def get_floor( node, ip )
-      target = IPAddr.new( ip ).to_i
-      return floor( node, target )
-    end
-
-    def floor( node, target )
-      if node == nil
-        return nil
-      else
-        if target == node.number
-          return node
-        else
-          if target < node.number
-            return floor( node.left, target )
-          else
-            f = floor( node.right, target )
-            if f
-              return f
-            else
-              return node
-            end
-          end
-        end
-      end
-    end
-
-  end
 
   class NetTree
+
+    NetNode = Struct.new( :cidr_length, :ipaddr, :data )
 
     attr_accessor :v4_root, :v6_root
 
     def initialize
-      @btree = NicInfo::NetBTree.new
+      @btree = NicInfo::BinarySearchTree.new
     end
 
     def append_to( l, n )
@@ -112,76 +37,81 @@ module NicInfo
       end
     end
 
-    def insert( cidr_string, data )
-
-      n = @btree.new_bnode( cidr_string, data )
-      if n.ipaddr.ipv4?
-        if @v4_root
-          s = @btree.lookup( @v4_root, n.number )
-          if s
-            if s.ipaddr.eql?( n.ipaddr )
-              raise "collision. s: #{s} n: #{n}"
-            else
-              s.data = append_to( s, n )
+    def insert_into_root( root, cidr_length, start_addr, ipaddr, data )
+      if root
+        s = @btree.lookup( root, start_addr )
+        if s
+          if s.data.is_a?( Array )
+            s.data.each do |i|
+              if i.ipaddr.eql?( ipaddr )
+                raise "collision. s: #{i} n: #{ipaddr}"
+              end
             end
+            #else
+            net_node = NetNode.new( cidr_length, ipaddr, data )
+            s.data = append_to( s.data, net_node )
+          elsif s.data.ipaddr.eql?( ipaddr )
+            raise "collision. s: #{s} n: #{ipaddr}"
           else
-            @btree.insert( @v4_root, n )
+            net_node = NetNode.new( cidr_length, ipaddr, data )
+            s.data = append_to( s.data, net_node )
           end
         else
-          @v4_root = n
+          net_node = NetNode.new( cidr_length, ipaddr, data )
+          @btree.insert( root, start_addr, net_node )
         end
       else
-        if @v6_root
-          s = @btree.lookup( @v6_root, n.number )
-          if s
-            if s.ipaddr.eql?( n.ipaddr )
-              raise "collision. s: #{s} n: #{n}"
-            else
-              s.data = append_to( s, n )
-            end
-          else
-            @btree.insert( @v6_root, n )
-          end
-        else
-          @v6_root = n
-        end
+        net_node = NetNode.new( cidr_length, ipaddr, data )
+        root = @btree.insert( root, start_addr, net_node )
+      end
+      return root
+    end
+
+    def insert( cidr_string, data )
+
+      ipaddr = IPAddr.new( cidr_string )
+      cidr_length = cidr_string.split( "/" )[1].to_i
+      start_addr = ipaddr.to_range.begin.to_i
+      if ipaddr.ipv4?
+        r = insert_into_root( @v4_root, cidr_length, start_addr, ipaddr, data )
+        @v4_root = r unless @v4_root
+      else
+        r = insert_into_root( @v6_root, cidr_length, start_addr, ipaddr, data )
+        @v6_root = r unless @v6_root
       end
 
     end
 
-    def find_by_ipaddr( ip )
+    def find_by_root( root, ipaddr )
       retval = nil
-      ipaddr = IPAddr.new( ip )
-      if ipaddr.ipv4?
-        n = @btree.floor( @v4_root, ipaddr.to_i )
-        if n
-          if n.is_a?( Array )
-            n.each do |i|
-              if i.ipaddr.include?( ipaddr )
-                retval = i.data
-                break
-              end
+      n = @btree.floor( root, ipaddr.to_i )
+      if n
+        if n.data.is_a?( Array )
+          n.data.each do |i|
+            if i.ipaddr.include?( ipaddr )
+              retval = i.data
+              break
             end
-          else
-            retval = n.data
           end
-        end
-      else
-        n = @btree.floor( @v6_root, ipaddr.to_i )
-        if n
-          if n.is_a?( Array )
-            n.each do |i|
-              if i.ipaddr.include?( ipaddr )
-                retval = i.data
-                break
-              end
-            end
-          else
-            retval = n.data
-          end
+        else
+          retval = n.data.data if n.data.ipaddr.include?( ipaddr )
         end
       end
       return retval
+    end
+
+    def find_by_ipaddr( ip )
+      ipaddr = IPAddr.new( ip )
+      if ipaddr.ipv4?
+        return find_by_root( @v4_root, ipaddr )
+      else
+        return find_by_root( @v6_root, ipaddr )
+      end
+    end
+
+    def each( &block )
+      @btree.each( @v4_root, &block )
+      @btree.each( @v6_root, &block )
     end
 
   end
--- spec/net_tree_spec.rb
+++ spec/net_tree_spec.rb
@@ -19,61 +19,27 @@ require_relative '../lib/nicinfo/net_tree'
 
 describe 'net_tree test' do
 
-  it 'should btree basics' do
-
-    t = NicInfo::NetBTree.new
-    root = t.insert( nil, NicInfo::NetBTree::BNode.new( nil, nil, 5, "a", nil, nil ) )
-    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 3, "b", nil, nil ) )
-    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 1, "c", nil, nil ) )
-    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 4, "d", nil, nil ) )
-    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 9, "e", nil, nil ) )
-    t.insert( root, NicInfo::NetBTree::BNode.new( nil, nil, 6, "f", nil, nil ) )
-    expect( t.lookup( root, 1 ).data ).to eq( "c" )
-    expect( t.lookup( root, 3 ).data ).to eq( "b" )
-    expect( t.lookup( root, 4 ).data ).to eq( "d" )
-    expect( t.lookup( root, 5 ).data ).to eq( "a" )
-    expect( t.lookup( root, 6 ).data ).to eq( "f" )
-    expect( t.lookup( root, 9 ).data ).to eq( "e" )
-    expect( t.floor( root, 1 ).data ).to eq( "c" )
-    expect( t.floor( root, 2 ).data ).to eq( "c" )
-    expect( t.floor( root, 3 ).data ).to eq( "b" )
-    expect( t.floor( root, 4 ).data ).to eq( "d" )
-    expect( t.floor( root, 5 ).data ).to eq( "a" )
-    expect( t.floor( root, 6 ).data ).to eq( "f" )
-    expect( t.floor( root, 7 ).data ).to eq( "f" )
-    expect( t.floor( root, 8 ).data ).to eq( "f" )
-    expect( t.floor( root, 9 ).data ).to eq( "e" )
-    expect( t.floor( root, 10 ).data ).to eq( "e" )
-
+  it 'append to array' do
+    t = NicInfo::NetTree.new
+    n1 = NicInfo::NetTree::NetNode.new( 24, IPAddr.new( "10.0.0.0/24" ), 1 )
+    n2 = NicInfo::NetTree::NetNode.new( 16, IPAddr.new( "10.0.0.0/16" ), 2 )
+    n3 = NicInfo::NetTree::NetNode.new( 8, IPAddr.new( "10.0.0.0/8" ), 3 )
+    n4 = NicInfo::NetTree::NetNode.new( 25, IPAddr.new( "10.0.0.0/8" ), 4 )
+    a = t.append_to( n3, n1 )
+    expect( a[0].cidr_length ).to eq( 24 )
+    expect( a[1].cidr_length ).to eq( 8 )
+    a = t.append_to( a, n2 )
+    expect( a[0].cidr_length ).to eq( 24 )
+    expect( a[1].cidr_length ).to eq( 16 )
+    expect( a[2].cidr_length ).to eq( 8 )
+    a = t.append_to( a, n4 )
+    expect( a[0].cidr_length ).to eq( 25 )
+    expect( a[1].cidr_length ).to eq( 24 )
+    expect( a[2].cidr_length ).to eq( 16 )
+    expect( a[3].cidr_length ).to eq( 8 )
   end
 
-  it 'should do net btree stuff' do
-
-    t = NicInfo::NetBTree.new
-    root = t.new_bnode( "0.0.0.0/0", "a" )
-    t.put( root, "10.0.0.0/16", "b")
-    t.put( root, "9.0.0.0/16", "c")
-    t.put( root, "8.0.0.0/16", "d")
-    t.put( root, "11.0.0.0/16", "e")
-    expect( t.get( root, "10.0.0.0" ).data ).to eq( "b" )
-    expect( t.get( root, "9.0.0.0" ).data ).to eq( "c" )
-    expect( t.get( root, "8.0.0.0" ).data ).to eq( "d" )
-    expect( t.get( root, "11.0.0.0" ).data ).to eq( "e" )
-    expect( t.get_floor( root, "11.0.0.0" ).data ).to eq( "e" )
-    expect( t.get_floor( root, "11.0.0.1" ).data ).to eq( "e" )
-    expect( t.get_floor( root, "8.0.0.0" ).data ).to eq( "d" )
-    expect( t.get_floor( root, "8.0.0.1" ).data ).to eq( "d" )
-    expect( t.get_floor( root, "9.0.0.0" ).data ).to eq( "c" )
-    expect( t.get_floor( root, "9.0.0.1" ).data ).to eq( "c" )
-    expect( t.get_floor( root, "10.0.0.0" ).data ).to eq( "b" )
-    expect( t.get_floor( root, "10.0.0.1" ).data ).to eq( "b" )
-    expect( t.get_floor( root, "10.0.0.2" ).data ).to eq( "b" )
-    expect( t.get_floor( root, "10.0.0.255" ).data ).to eq( "b" )
-    expect( t.get_floor( root, "10.255.255.255" ).data ).to eq( "b" )
-
-  end
-
-  xit 'should insert' do
+  it 'should insert' do
 
     t = NicInfo::NetTree.new
     t.insert( "10.0.0.0/24", 1 )
